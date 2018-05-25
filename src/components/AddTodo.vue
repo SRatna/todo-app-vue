c43719da6473fe7e709e9b0a1d315b3da7d015c7
sratna <adhikariratna52@gmail.com>
2018-05-25 09:39:35 +0545

    expand add todo component with adding n viewing todos

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,12 +1,41 @@
 <template>
-  <input type="text">
+  <div>
+    <input type="text" v-model="todoText" @keyup.enter="onTodoEntered">
+    <ul>
+      <li v-for="(todo, index) in todos" :key="todo.id">
+      <span>
+        {{index + 1}}. {{todo.text}}
+      </span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'AddTodo',
+  import { mapActions, mapState } from 'vuex'
 
-}
+  export default {
+    name: 'AddTodo',
+    data: function () {
+      return {
+        todoText: ''
+      }
+    },
+    methods: {
+      ...mapActions([
+        'addTodo'
+      ]),
+      onTodoEntered: function () {
+        this.addTodo(this.todoText);
+        this.todoText = '';
+      }
+    },
+    computed: {
+      ...mapState([
+        'todos'
+      ])
+    }
+  }
 </script>
 
 <style scoped lang="scss">

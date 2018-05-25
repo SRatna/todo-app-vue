0db26734318c235767ec5345571111446da83fbb
sratna <adhikariratna52@gmail.com>
2018-05-25 15:14:39 +0545

    add todo list component and render todo component in it

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <Todo :todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+  import Todo from './Todo.vue'
+
+  export default {
+    name: 'TodoList',
+    components: {
+      Todo,
+    },
+    data: function () {
+      return {
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      ...mapState([
+        'todos'
+      ])
+    }
+  }
+</script>

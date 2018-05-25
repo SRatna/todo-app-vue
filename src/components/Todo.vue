e84c97b2994d403d08c32f941a0b142564a56034
sratna <adhikariratna52@gmail.com>
2018-05-25 15:14:17 +0545

    add todo list item component with features like toggling completion status n removing todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-task">
+    <input
+      @click="toggleTodo(todo.id)"
+      type="checkbox"
+      class="todo-checkbox"> &nbsp;
+    <span
+      class="todo-text"
+      :class="{ 'todo-task-done': todo.completed }">
+        {{todo.text}}
+    </span>
+    <span
+      @click="removeTodo(todo.id)"
+      class="todo-cross">
+      &#10005;
+    </span>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+
+  export default {
+    name: 'Todo',
+    props: {
+      todo: Object
+    },
+    data: function () {
+      return {
+      }
+    },
+    methods: {
+      ...mapActions([
+        'toggleTodo',
+        'removeTodo',
+      ])
+    },
+    computed: {
+
+    }
+  }
+</script>

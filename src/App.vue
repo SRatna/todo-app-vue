43e007064fd16985b9aee047178c4a6b05f6423c
sratna <adhikariratna52@gmail.com>
2018-05-28 10:17:12 +0545

    add footer component in main root component

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <p class="title">todos</p>
     <AddTodo />
     <TodoList />
+    <Footer />
   </div>
 </template>
 
 <script>
 import AddTodo from './components/AddTodo.vue'
 import TodoList from './components/TodoList.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
     AddTodo,
-    TodoList
+    TodoList,
+    Footer
   }
 }
 </script>

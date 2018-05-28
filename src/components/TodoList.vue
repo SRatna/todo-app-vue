51ac245510979062b2afc8262cc3918d5177bc7c
sratna <adhikariratna52@gmail.com>
2018-05-28 10:20:09 +0545

    show todos according to filter status

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <div v-for="todo in todos" :key="todo.id">
-      <Todo :todo="todo" />
+    <div v-for="todo in filteredTodos" :key="todo.id">
+      <Todo v-if="beingEditedTodo.id !== todo.id" :todo="todo" />
+      <EditTodo v-else :todo="todo" />
     </div>
   </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
   import { mapState } from 'vuex'
   import Todo from './Todo.vue'
+  import EditTodo from './EditTodo.vue'
 
   export default {
     name: 'TodoList',
     components: {
       Todo,
+      EditTodo
     },
     data: function () {
       return {
@@ -24,8 +27,19 @@
     },
     computed: {
       ...mapState([
-        'todos'
-      ])
+        'todos',
+        'beingEditedTodo',
+        'filter'
+      ]),
+      filteredTodos: function () {
+        if (this.filter.name === 'SHOW_ACTIVE') {
+          return this.todos.items.filter(todo => todo.completed === false);
+        }
+        if (this.filter.name === 'SHOW_DONE') {
+          return this.todos.items.filter(todo => todo.completed === true);
+        }
+        return this.todos.items;
+      }
     }
   }
 </script>
